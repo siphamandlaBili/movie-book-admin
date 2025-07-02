de4f3c7fe80c6806305807de5d8c7756072bd374
Siphamandla <siphabili@gmail.com>
2025-07-02 14:52:54 +0200

    function inngest test

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -10,10 +10,10 @@ const syncUserCreation = inngest.createFunction(
     {event:"clerk/user.created"},
 
     async (event)=>{
-      const {first_name,last_name,email_addresses,image_url} = event.data;
+      const {id,first_name,last_name,email_addresses,image_url} = event[0]?.data;
 
       const userData = {
-        _id:1,
+        _id:id,
         name:`${first_name} ${last_name}`,
         email:email_addresses[0]?.email_address,
         image:image_url
